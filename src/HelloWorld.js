bdf2c297b0962bf63d66479e6f2cc2b80c23fbc5
Davi Ortega <ortegad@caltech.edu>
2017-02-13 18:00:30 -0800

    trying to commit with error

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -6,6 +6,9 @@ class HelloWord {
 		this.heyThere = 'Hello, '
 	}
 	to(person) {
-		return this.heyThere + person + '!'
+		return this.heyThere + person
+	}
+	orNot(person) {
+		return 'Not ' + this.heyThere + ' for ' + person
 	}
 }
