0b7b9b55aad0fe0d48f78dfdda40863a38ab7776
Tony Kukavica <tonykukavica@Tonys-MacBook-Air-2.local>
2017-03-02 18:08:01 -0800

    headers method

--- src/FastaReader.js
+++ src/FastaReader.js
@@ -11,11 +11,12 @@ class FastaReqder {
 		let toPush = ''
 		for (let step1 = 0; step1 < chars.length; step1++) {
 			if (chars[step1] === '>') {
-				while (chars[step1 + 1] !== '\\') {
+				while (chars[step1 + 1] !== '\n' && step1 + 1 < chars.length) {
 					toPush += chars[step1 + 1]
 					step1++
 				}
 				this.headers.push(toPush)
+				toPush = ''
 			}
 		}
 		return 0
