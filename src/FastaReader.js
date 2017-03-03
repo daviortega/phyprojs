1f9bc3b00aec5d06534a347a59879edba9e33e98
Tony Kukavica <tonykukavica@Tonys-MacBook-Air-2.local>
2017-03-02 18:11:39 -0800

    sequences

--- src/FastaReader.js
+++ src/FastaReader.js
@@ -12,6 +12,8 @@ class FastaReqder {
 		let sequencesToPush = ''
 		for (let x = 0; x < chars.length; x++) {
 			if (chars[x] === '>') {
+				if (sequencesToPush !== '')
+					this.sequences.push(sequencesToPush)
 				while (chars[x + 1] !== '\n' && x + 1 < chars.length) {
 					headersToPush += chars[x + 1]
 					x++
