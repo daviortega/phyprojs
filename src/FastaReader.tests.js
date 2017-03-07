226e211b29e34b106809531a032033bac02ba0ff
Tony Kukavica <tonykukavica@Tonys-MacBook-Air-2.local>
2017-03-06 17:21:39 -0800

    catching errors

--- src/FastaReader.tests.js
+++ src/FastaReader.tests.js
@@ -48,7 +48,7 @@ describe('Fasta reader test suite', function() {
 			expect(fr.sequences).to.eql(expectedSequences)
 		})
 	})
-	describe('Caching Errors ', function() {
+	describe('Catching Errors ', function() {
 		it('no ">" at the first character should make invalid FASTA', function() {
 			let fr = new FastaReader(),
 				fasta = 'header1\nDAVIDA\n\nVIDAVIIIIIII\n>header2\nDDDAA\nAVVVIII\n'
